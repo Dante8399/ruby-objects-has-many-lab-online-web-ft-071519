28710b598d41256c0205088e76397cd49a11a02a
Dante Spencer <spencerdantel@gmail.com>
2019-08-02 12:34:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,25 @@
-class Artist
- attr_accessor :name
+  class Artist
+  attr_accessor :name
+
+  @@all = []
+
   def initialize(name)
     @name = name
-    @songs = []
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
   def add_song(song)
-    @songs << song
     song.artist = self
   end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+  end
+end
   
     
